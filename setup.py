a5f47f55eeda7838dae5fa414da59c186410c54c
chaupham <chau.datascience@gmail.com>
2022-08-18 00:23:20 -0400

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name="clip",
     py_modules=["clip"],
-    version="1.0.1",
+    version="1.0.2",
     description="",
     author="OpenAI",
     packages=find_packages(exclude=["tests*"]),
