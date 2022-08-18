81c793a5788585022a5f3c5bff6d2b617b0dcc37
chaupham <chau.datascience@gmail.com>
2022-08-18 01:13:10 -0400

    update

--- clip/model.py
+++ clip/model.py
@@ -355,9 +355,8 @@ class CLIP(nn.Module):
             x = x[torch.arange(x.shape[0]), text.argmax(dim=-1)] @ self.text_projection
             return x
         else:
-            ### edit
-            tokens_len = text.argmax(dim=-1) + 1
-            max_token_len = max(tokens_len).item() + 1
+            tokens_len = text.argmax(dim=-1)+1
+            max_token_len = max(text.argmax(dim=-1)).item()+1
 
             x = x[torch.arange(x.shape[0]), :max_token_len] @ self.text_projection
             # x shape: batch, max_token_len, embed_dim
